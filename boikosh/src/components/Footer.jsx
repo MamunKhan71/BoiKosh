4dd2463c3202d9ef1293cd82ef041bff4fadadb3
Md. Mamun <mkmamun031@gmail.com>
2024-10-14 12:49:23 +0600

    Added Navbar and footer

---
+++ boikosh/src/components/Footer.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className="bg-[#181C14]">
+            <div className="container py-12 mx-auto">
+                <div className="flex flex-col items-center text-center">
+                    <a href="#">
+                        <img className='w-48' src="https://res.cloudinary.com/dfwmhlhyo/image/upload/v1728887407/boikosh_cwrtzw.png" alt="" />
+                    </a>
+                    <p className="max-w-md mx-auto mt-4 text-gray-500 dark:text-gray-400">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+
+                </div>
+                <hr className="my-10 border-gray-200 dark:border-gray-700" />
+                <div className="flex flex-col items-center sm:flex-row sm:justify-between">
+                    <p className="text-md text-white">
+                        © Copyright 2024. All Rights Reserved by Md. Mamun with love for <span className='font-bold text-amber-400'>ZeptoApps!</span>
+                    </p>
+                    <div className="flex mt-3 -mx-2 sm:mt-0">
+                        <a
+                            href="#"
+                            className="mx-2 text-md transition-colors duration-300 text-white"
+                        >
+                            {" "}
+                            Teams{" "}
+                        </a>
+                        <a
+                            href="#"
+                            className="mx-2 text-md transition-colors duration-300 text-white"
+                        >
+                            {" "}
+                            Privacy{" "}
+                        </a>
+                        <a
+                            href="#"
+                            className="mx-2 text-md transition-colors duration-300 text-white"
+                        >
+                            {" "}
+                            Cookies{" "}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer
--- boikosh/src/layout/MainLayout.jsx
+++ boikosh/src/layout/MainLayout.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const MainLayout = () => {
     return (
-        <div className='container mx-auto mt-4'>
-            {/* navbar */}
-            <Outlet />
-            {/* footer */}
+        <div className='relative space-y-7 font-primary text-lg'>
+            <div class="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+            <div className='h-screen container mx-auto mt-4  space-y-7 '>
+                <Navbar />
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     )
 }
